72804554506ea688cef038167f6296d4e9a1d61d
Jan Verschelde <janv@uic.edu>
2021-06-05 10:59:37 -0500

    Update push and pop procedure to work with multiple producers and multiple consumers.

--- Synchronization/src/float64_queue.adb
+++ Synchronization/src/float64_queue.adb
@@ -23,50 +23,68 @@ package body Float64_Queue is
    end Initialize;  
 
    procedure push_back ( nbr : in Float64 ) is
+
+      fail : boolean := false;
+
    begin
       if full_queue then
          raise queue_full;
-      elsif front = 0 then
-         data(1) := nbr;
-         front := 1; back := 1;
-         full_queue := (data'last = 1);
       else
          Semaphore.Request(sem);
-         if back = data'last
-          then back := 1;
-          else back := back + 1;
-         end if;
-         data(back) := nbr;
-         if back = data'last
-          then full_queue := (front = 1);
-          else full_queue := (front = back+1);
+         if full_queue then
+            fail := true;
+	 elsif front = 0 then
+            data(1) := nbr;
+            front := 1; back := 1;
+            full_queue := (data'last = 1);
+         else
+            if back = data'last
+             then back := 1;
+             else back := back + 1;
+            end if;
+            data(back) := nbr;
+            if back = data'last
+             then full_queue := (front = 1);
+             else full_queue := (front = back+1);
+            end if;
          end if;
          Semaphore.Release(sem);
+         if fail
+          then raise queue_full;
+         end if;
       end if;
    end push_back;
 
    function pop_front return Float64 is
 
       result : Float64 := 0.0;
+      fail : boolean := false;
 
    begin
       if front = 0 then
          raise queue_empty;
       else
          Semaphore.Request(sem);
-         result := data(front);
-         if front = back then
-            front := 0;
+	 if front = 0 then
+            fail := true;
          else
-            front := front + 1;
-            if front > data'last
-             then front := 1;
-            end if;
-            if full_queue
-             then full_queue := false;
+            result := data(front);
+            if front = back then
+               front := 0;
+            else
+               front := front + 1;
+               if front > data'last
+                then front := 1;
+               end if;
+               if full_queue
+                then full_queue := false;
+               end if;
             end if;
          end if;
          Semaphore.Release(sem);
+         if fail
+          then raise queue_empty;
+         end if;
       end if;
       return result;
    end pop_front;
